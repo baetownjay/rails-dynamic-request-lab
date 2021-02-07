80c0643f6c68ee34519a0665e4374659a6235207
Jason Herrera <baytownjason@yahoo.com>
2021-02-06 21:43:10 -0600

    finished

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  def show
+    @student = Student.find_by(id: params[:id])
+    if @student
+      render :show
+    else
+      redirect_to :index
+    end
+  end
 end
